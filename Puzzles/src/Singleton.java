511602cb9b72e28a09dc00adcb6ffff136bf8340
Sagar Lad <sslad1988@gmail.com>
2020-03-30 14:48:49 +0530

    Add files via upload

---
+++ Puzzles/src/Singleton.java
@@ -0,0 +1,22 @@
+
+public class Singleton {
+
+	private static Singleton singleton;
+
+	private Singleton() {
+
+	}
+
+	public static Singleton getInstance() {
+		if (singleton == null) {
+			synchronized (Singleton.class) {
+				if (singleton == null) {
+					singleton = new Singleton();
+				}
+			}
+
+		}
+		return singleton;
+	}
+
+}
